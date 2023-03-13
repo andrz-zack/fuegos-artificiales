77f062a1b8a9e226c413a132d1e5156ebff49deb
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-13 17:36:46 -0500

    Realizando la function draw

--- js/main.js
+++ js/main.js
@@ -18,4 +18,16 @@ function draw(){
      
      let foo, i, j, r;
      foo = Math.sin(t) * 2 * Math.PI;
+     for (i = 0; i < 400; ++i){
+        r = 400 * Math.sin(i * foo);
+        $.globalCompositeOperation = '';
+        $.fillStyle = 'hsla('+i + 12 + ', 100%, 60%, 1)';
+        $.beginPath();
+        $.arc(Math.sin(i) * r + (c.width / 2),
+            Math.cos(i) * r + (c.height / 2),
+            1.5, 0, Math.PI * 2);
+        $.fill();
+     }
+     t += 0.000004;
+     return t %= 2 * Math.PI;
 }
