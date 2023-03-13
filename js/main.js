f5c0909372c7fb7095832ddd3b6610255a3f68b1
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-13 17:28:05 -0500

    for

--- js/main.js
+++ js/main.js
@@ -3,10 +3,19 @@ let c = document.querySelector("canvas");
 let $ = c.getContext('2d');
 c.width = window.innerWidth;
 c.height = window.innerHeight;
-$.fillStyle = 'hsla(0, 0%, 0%, 1)';
+$.fillStyle = 'hsla(0, 0%, 0%, .1)';
 
 window.addEventListener('resize', function(){
     c.width = window.innerWidth;
     c.height = window.innerHeight;
 
 }, false);
+
+function draw(){
+     $.globalCompositeOperation = 'source-over';
+     $.fillStyle = 'hsla(0, 0%, 0%, .1)';
+     $.fillRect(0, 0, c.width, c.height);
+     
+     let foo, i, j, r;
+     foo = Math.sin(t) * 2 * Math.PI;
+}
