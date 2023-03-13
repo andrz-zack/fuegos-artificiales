8b812b3ec5b361233d22478406cbf69f61cbf32c
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-13 17:06:46 -0500

    Agregando funcionalidad

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ let $ = c.getContext('2d');
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 $.fillStyle = 'hsla(0, 0%, 0%, 1)';
+
+window.addEventListener('resize', function(){
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+
+}, false);
